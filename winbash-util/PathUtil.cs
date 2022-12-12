89f77df3141c004317bdb21598e94ba9beb79816
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-12 21:11:27 +0100

    add util lib

---
+++ winbash-util/PathUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace winbash.util;
+
+public static class PathUtil
+{
+    public static string Unwrap(string path)
+    {
+        bool abs = Regex.IsMatch(path, "([A-Z]:)?/[a-zA-Z0-9-_./]*");
+        path = path.Replace("~", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+        if (!abs)
+            path = Path.Combine(Environment.CurrentDirectory, path);
+        return path;
+    }
+
+    public static string Wrap(string path)
+    {
+        return path.Replace(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "~");
+    }
+}
