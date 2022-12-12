0925d22ea20f632fdf33c38b10eb1e4f72914ca1
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-12 21:11:39 +0100

    implement cd command and add color coding

--- winbash/Program.cs
+++ winbash/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace winbash;
+﻿using winbash.util;
+
+namespace winbash;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static void Main(string[] _)
     {
-        Console.WriteLine("Hello, world!");
+        bool exiting = false;
+        
+        AnsiUtil.Init();
+
+        while (!exiting)
+        {
+            Console.Write($"{AnsiUtil.Green}{Environment.UserName}@{Environment.UserDomainName} {AnsiUtil.Blue}{PathUtil.Wrap(Environment.CurrentDirectory)} $ {AnsiUtil.Reset}");
+            var cmd = Console.ReadLine();
+            if (string.IsNullOrEmpty(cmd))
+                continue;
+            var cmds = cmd.Split(" ");
+            if (cmds.Length == 0)
+                continue;
+            var args = new string[cmds.Length - 1];
+            Array.Copy(cmds, 1, args, 0, args.Length);
+            switch (cmds[0])
+            {
+                case "exit":
+                    exiting = true;
+                    break;
+                case "ls":
+                    break;
+                case "cd":
+                    var path = string.Join(" ", args);
+                    path = PathUtil.Unwrap(path);
+                    if (path == null || !Directory.Exists(path))
+                    {
+                        Console.WriteLine("Not a directory: " + path);
+                        break;
+                    }
+                    Environment.CurrentDirectory = path;
+                    break;
+            }
+        }
     }
 }
