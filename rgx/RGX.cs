be0651b0454a93d2cd5c1e33711c1eb9e573aabb
burdoto <kaleidox@comroid.org>
2023-05-07 09:03:19 +0200

    fix split stream

--- rgx/RGX.cs
+++ rgx/RGX.cs
@@ -8,7 +8,6 @@ namespace winbash.rgx;
 public static class RGX
 {
     private static readonly ILog log = new Log("rgx");
-    private static string[] excessArgs = Array.Empty<string>();
 
     static RGX()
     {
@@ -17,33 +16,35 @@ public static class RGX
 
     public static void Main(string[] args)
     {
-        var result = CLI.parser.ParseArguments<Arg>(args);
-        excessArgs = result.MapResult(_ => Enumerable.Empty<string>(), 
-            errs => errs.OfType<UnknownOptionError>().Select(e => e.Token)).ToArray();
-        result.WithParsed(Run).WithNotParsed(Error);
+        CLI.parser.ParseArguments<Arg>(args)
+            .WithParsed(Run)
+            .WithNotParsed(Error);
     }
 
     private static void Run(Arg cmd)
     {
         var pattern = new Regex(cmd.pattern, (RegexOptions)cmd.options.Aggregate(0, (x, y) => x | (int)y));
-        using var input = excessArgs.Length > 0 ? new StringReader(string.Join(' ', excessArgs)) : Console.In;
+        using var input = cmd is { split: true, replacement: not null } ? new StringReader(cmd.replacement) : Console.In;
         using var output = cmd.fileOutput is not null ? new StreamWriter(new FileStream(cmd.fileOutput, FileMode.Truncate, FileAccess.Write)) : Console.Out;
 
         if (cmd.split)
         {
-            if (cmd.pattern.StartsWith('^'))
+            var newPattern = cmd.pattern;
+            if (newPattern.StartsWith('^'))
                 // do not match beginning of string
-                pattern = new Regex(cmd.pattern.Substring(1));
-            int c,r=0;
+                newPattern = newPattern.Substring(1);
+            if (!newPattern.EndsWith('$'))
+                newPattern += '$';
+            pattern = new Regex(newPattern);
+            int c;
             var buf = new StringWriter();
-            var maxLen = pattern.ToString().Length * 4;
             while ((c = input.Read()) != -1)
             {
-                buf.Write(c);
-                output.Write(c);
-                r += 1;
+                buf.Write((char)c);
+                output.Write((char)c);
 
-                if (!pattern.IsMatch(buf.ToString()) || r > maxLen)
+                var str = buf.ToString();
+                if (!pattern.IsMatch(str))
                     continue;
                 buf.Close();
                 buf = new StringWriter();
@@ -68,6 +69,8 @@ public static class RGX
 
         [Value(1, MetaName = "replacement", Required = false, Default = null)]
         public string? replacement { get; set; }
+        [Value(1)]
+        public IEnumerable<string> excess { get; set; }
 
         [Option(shortName: 'o', longName: "options", Separator = ',', Required = false)]
         public IEnumerable<RegexOptions> options { get; set; } = null!;
